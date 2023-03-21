7df02260a9d8a09a095eea63ada3777460dae2b3
wololo <uz21827@bristol.ac.uk>
2023-03-21 16:27:06 +0000

    Added next steps, removed what we dont need, adjusted the grids to work, cleaned up the code slightly to improve readability

--- cw3.py
+++ cw3.py
@@ -2,63 +2,71 @@ import random
 import copy
 import time
 
+
+'''
+NOTES:
+- To do Task 1.1 edit in 'recursive solve'
+- To do adjust the functions to work with grids other than 2x2,
+	currently they only work with 2x2 and so get stuck at grid 5
+'''
+
 grid1 = [
-		[1, 1, 1, 1],
-		[1, 1, 1, 1],
-		[1, 1, 1, 1],
-		[1, 1, 1, 1]]
-
-grid2 = [
 		[1, 0, 4, 2],
-		[4, 2, 1, 3],
-		[0, 1, 0, 4],
-		[3, 4, 2, 1]]
-
-grid3 = [
-		[1, 2, 3, 4],
-		[2, 1, 4, 3],
-		[3, 4, 2, 1],
-		[4, 3, 1, 2]]
-
-grid4 = [
-		[1, 3, 4, 2],
 		[4, 2, 1, 3],
 		[2, 1, 3, 4],
 		[3, 4, 2, 1]]
 
+grid2 = [
+		[1, 0, 4, 2],
+		[4, 2, 1, 3],
+		[2, 1, 0, 4],
+		[3, 4, 2, 1]]
+
+grid3 = [
+		[1, 0, 4, 2],
+		[4, 2, 1, 0],
+		[2, 1, 0, 4],
+		[0, 4, 2, 1]]
+
+grid4 = [
+		[1, 0, 4, 2],
+		[0, 2, 1, 0],
+		[2, 1, 0, 4],
+		[0, 4, 2, 1]]
+
 #Grids 4-7 are 3x3
 grid5 = [
-		[1, 2, 3, 4, 5, 6, 7, 8, 9,],
-		[2, 3, 4, 5, 6, 7, 8, 9, 1,],
+		[1, 2, 3, 4, 5, 0, 7, 8, 9,],
+		[2, 3, 4, 0, 6, 7, 8, 9, 1,],
 		[3, 4, 5, 6, 7, 8, 9, 1, 2,],
 		[4, 5, 6, 7, 8, 9, 1, 2, 3,],
-		[5, 6, 7, 8, 9, 1, 2, 3, 4,],
+		[5, 6, 0, 8, 9, 1, 2, 0, 4,],
 		[6, 7, 8, 9, 1, 2, 3, 4, 5,],
-		[7, 8, 9, 1, 2, 3, 4, 5, 6,],
+		[0, 8, 0, 1, 2, 0, 4, 5, 6,],
 		[8, 9, 1, 2, 3, 4, 5, 6, 7,],
 		[9, 1, 2, 3, 4, 5, 6, 7, 8,]]
 
 grid6 = [
 		[6, 1, 7, 8, 4, 2, 5, 3, 9,],
-		[7, 4, 5, 3, 6, 9, 1, 8, 2,],
+		[7, 4, 5, 3, 6, 9, 0, 8, 2,],
 		[8, 3, 2, 1, 7, 5, 4, 6, 9,],
 		[1, 5, 8, 6, 9, 7, 3, 2, 4,],
-		[9, 6, 4, 2, 3, 1, 8, 7, 5,],
+		[9, 6, 4, 0, 3, 1, 8, 7, 5,],
 		[2, 7, 3, 5, 8, 4, 6, 9, 1,],
-		[4, 8, 7, 9, 5, 6, 2, 1, 3,],
+		[4, 8, 7, 9, 5, 6, 2, 0, 3,],
 		[3, 9, 1, 4, 2, 8, 7, 5, 6,],
-		[5, 2, 6, 7, 1, 3, 9, 4, 8,]]
+		[5, 2, 6, 7, 1, 0, 9, 4, 8,]]
 
 grid7 = [
-		[6, 1, 9, 8, 4, 2, 5, 3, 7,],
-		[7, 4, 5, 3, 6, 9, 1, 8, 2,],
-		[8, 3, 2, 1, 7, 5, 4, 6, 9,],
-		[1, 5, 8, 6, 9, 7, 3, 2, 4,],
+		[6, 1, 9, 8, 4, 0, 5, 3, 7,],
+		[7, 0, 5, 3, 6, 9, 1, 8, 2,],
+		[8, 3, 0, 1, 7, 5, 4, 6, 9,],
+		[1, 5, 8, 6, 9, 7, 3, 0, 4,],
 		[9, 6, 4, 2, 3, 1, 8, 7, 5,],
-		[2, 7, 3, 5, 8, 4, 6, 9, 1,],
+		[2, 7, 0, 5, 8, 4, 6, 9, 1,],
 		[4, 8, 7, 9, 5, 6, 2, 1, 3,],
 		[3, 9, 1, 4, 2, 8, 7, 5, 6,],
-		[5, 2, 6, 7, 1, 3, 9, 4, 8,]]
+		[5, 2, 6, 0, 1, 3, 9, 4, 8,]]
 
 #grids 8-10 are 2x3
 grid8 = [
@@ -70,31 +78,26 @@ grid8 = [
 		[0, 5, 0, 0, 6, 4]]
 
 grid9 = [
-		[1, 2, 6, 5, 4, 3],
-		[5, 3, 4, 6, 2, 1],
-		[6, 1, 3, 4, 5, 2],
-		[2, 5, 5, 3, 1, 6],
-		[4, 6, 1, 2, 3, 5],
+		[1, 0, 6, 5, 4, 3],
+		[5, 3, 0, 6, 2, 1],
+		[6, 0, 3, 0, 5, 2],
+		[2, 5, 5, 3, 0, 6],
+		[4, 0, 1, 2, 3, 0],
 		[3, 5, 2, 1, 6, 4]]
 
 grid10 = [
-		[1, 2, 6, 5, 4, 3],
-		[5, 3, 4, 6, 2, 1],
-		[6, 1, 3, 4, 5, 2],
-		[2, 4, 5, 3, 1, 6],
+		[1, 0, 6, 5, 4, 3],
+		[5, 3, 4, 6, 0, 1],
+		[0, 1, 3, 4, 5, 2],
+		[2, 0, 5, 0, 1, 6],
 		[4, 6, 1, 2, 3, 5],
-		[3, 5, 2, 1, 6, 4]]
+		[0, 5, 2, 1, 0, 4]]
 
 
 grids = [(grid1, 2, 2), (grid2, 2, 2), (grid3, 2, 2), (grid4, 2, 2), 
 		(grid5, 3, 3), (grid6, 3, 3), (grid7, 3, 3),
 		(grid8, 2, 3), (grid9, 2, 3), (grid10, 2, 3)]
 
-'''
-===================================
-DO NOT CHANGE CODE ABOVE THIS LINE
-===================================
-'''
 
 def check_section(section, n):
 
@@ -115,10 +118,8 @@ def get_squares(grid, n_rows, n_cols):
 				square +=line
 			squares.append(square)
 
-
 	return(squares)
 
-#To complete the first assignment, please write the code for the following function
 def check_solution(grid, n_rows, n_cols):
 	'''
 	This function is used to check whether a sudoku board has been correctly solved
@@ -162,58 +163,7 @@ def find_empty(grid):
 		for j in range(len(row)):
 			if grid[i][j] == 0:
 				return (i, j)
-
 	return None
-
-def remove_possibilities(grid): #edit this to remove the stuff from the rows and columns
-
-	#grids[] tuple deconstruction
-    (grid, box_count_x, box_count_y) = grid
-
-	#check for 0 values and fail if the case (incomplete answer)
-    for row in grid:
-        for entry in row:
-            if entry == 0:
-                return False
-    
-	#rows
-	#make a list of False values 10 long
-	#look through the values, if its seen mark as true
-	#if value comes up again then fail as cant repeat
-    for row in grid:
-        seen = [False] * 10
-        for entry in row:
-            if seen[entry]:
-                return False
-            seen[entry] = True
-    
-	#columns
-	#does the same as above but for columns
-    for y in range(0, len(grid)):
-        seen = [False] * 10
-        for x in range(0, len(grid[y])):
-            entry = grid[y][x]
-            if seen[entry]:
-                return False
-            seen[entry] = True
-
-	#much like the above but this time checking it as per the 2x2, 2x3
-	#or 3x3 boxes rather than rows or columns
-    box_height = round(len(grid) / box_count_y)
-    box_width = round(len(grid[0]) / box_count_x)
-    
-    for base_Y in range(0, len(grid), box_height):
-        for base_X in range(0, len(grid[base_Y]), box_width):
-            seen = [False] * 10
-            for box_Y in range(0, box_height):
-                for box_X in range(0, box_width):
-                    entry = grid[base_Y + box_Y][base_X + box_X]
-                    if seen[entry]:
-                        return False
-                    seen[entry] = True
-    
-	#if it hasnt failed on the other tests then mark as correct
-    return True
 
 
 def recursive_solve(grid, n_rows, n_cols):
@@ -241,6 +191,10 @@ def recursive_solve(grid, n_rows, n_cols):
 	else:
 		row, col = empty 
 
+
+	'''
+	This part below is what we want to change for Task 1.1
+	'''
 	#Loop through possible values
 	for i in range(1, n+1):
 
@@ -258,28 +212,6 @@ def recursive_solve(grid, n_rows, n_cols):
 
 	#If we get here, we've tried all possible values. Return none to indicate the previous value is incorrect.
 	return None
-
-
-def fill_board_randomly(grid, n_rows, n_cols):
-	'''
-	This function will fill an unsolved Sudoku grid with random numbers
-
-	args: grid, n_rows, n_cols
-	return: A grid with all empty values filled in
-	'''
-	n = n_rows*n_cols
-	#Make a copy of the original grid
-	filled_grid = copy.deepcopy(grid)
-
-	#Loop through the rows
-	for i in range(len(grid)):
-		#Loop through the columns
-		for j in range(len(grid[0])):
-			#If we find a zero, fill it in with a random integer
-			if grid[i][j] == 0:
-				filled_grid[i][j] = random.randint(1, n)
-
-	return filled_grid 
 
 def solve(grid, n_rows, n_cols):
 	return recursive_solve(grid, n_rows, n_cols)
